82baf79798398d77b86d11aacdeffcf51dc5fc32
tiff4ni <tiffani.bastidas03@protonmail.com>
2025-04-25 15:50:55 -0400

    Proyecto Django para Render

--- codigoBalanza/urls.py
+++ codigoBalanza/urls.py
@@ -1,8 +1,10 @@
+from django.shortcuts import redirect
 from django.urls import include
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('', lambda request: redirect('/productos/buscar/')),  
     path('admin/', admin.site.urls),
     path('productos/', include('productos.urls')),
 ]
--- productos/urls.py
+++ productos/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.shortcuts import redirect
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
@@ -7,6 +8,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('buscar/', views.buscar_producto, name='buscar_producto'),
     path('cerrar-sesion/', views.cerrar_sesion, name='cerrar_sesion'),
-     path('editar/<str:producto_id>/', views.editar_producto, name='editar_producto'),
+    path('editar/<str:producto_id>/', views.editar_producto, name='editar_producto'),
     path('eliminar/<str:producto_id>/', views.eliminar_producto, name='eliminar_producto'),
 ]
